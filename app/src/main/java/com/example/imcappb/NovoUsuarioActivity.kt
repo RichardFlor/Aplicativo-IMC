7b6192229f17150d3a8e5a0438757a5c2bccfb60
RICHARD MATHEUS FLOR DOROTEIO <21194072@CFP127ALUNO.LOCAL>
2021-11-16 14:54:50 -0300

    Cadastrando

--- app/src/main/java/com/example/imcappb/NovoUsuarioActivity.kt
+++ app/src/main/java/com/example/imcappb/NovoUsuarioActivity.kt
@@ -3,11 +3,21 @@ package com.example.imcappb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+
 
 class NovoUsuarioActivity : AppCompatActivity() {
+
+    lateinit var editEmail : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_novo_usuario)
+
+        editEmail =  findViewById(R.id.edit_novo_email)
+
+
     }
 
   override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -16,5 +26,20 @@ class NovoUsuarioActivity : AppCompatActivity() {
 
     return true
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        // Salvar os dados do usuário em um sharedPreferences
+        // Criar um arquivo sharedPreferences
+        // Se o arquivo já existir o arquivo será aberto
+        val dados = getSharedPreferences("cadastro", MODE_PRIVATE)
+
+        // Criar um editor para o arquivo
+        val editor = dados.edit()
+        editor.putString("email", editEmail.text.toString())
+        editor.apply()
+
+        return true
     }
 }
